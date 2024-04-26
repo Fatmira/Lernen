e0f481f391dee5fc6fe96bac2ed955d294e2cecd
AT <68025214+Fatmira@users.noreply.github.com>
2024-04-26 14:57:51 +0200

    Create lernen.py

---
+++ lernen.py
@@ -0,0 +1,21 @@
+from time import sleep
+
+#Datentypen
+String = "3" #Variable in string
+Integer = 3 #Variable in integer
+Float = 1.23 #Variable in float 
+
+
+#Wie man eine Variable einsetzen kann
+name = ("Peter") #string
+print(name , " ist groß") # -> Ausgabe: Peter ist groß
+
+alter = ("36") #string
+print(name, " ist ", alter, " Jahre alt") # -> Ausgabe: Peter ist 36 Jahre alt
+
+#Wie man input verwendet
+name2 = input("Gebe deinen Namen ein: ") #string
+print("Dein Name ist: ", name2 ) # -> Ausgabe: Dein name ist: (deine Eingabe)
+
+sekunden = float(input("In welcher Sekunde hat die stoppuhr gestopt? : ") #float
+print("Sie stopte bei: ", sekunden) # -> Ausgabe: Sie stopte bei (deiner Eingabe) 
